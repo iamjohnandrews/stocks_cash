411a33bc35b8e4dd9178cf9110a5b460dfd33707
John Andrews <john@soothsayerlabs.com>
2024-11-22 12:30:56 -0600

    Initial Commit

---
+++ stock_cash/stock_cashApp.swift
@@ -0,0 +1,17 @@
+//
+//  stock_cashApp.swift
+//  stock_cash
+//
+//  Created by John Andrews on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct stock_cashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ stock_cashTests/stock_cashTests.swift
@@ -0,0 +1,17 @@
+//
+//  stock_cashTests.swift
+//  stock_cashTests
+//
+//  Created by John Andrews on 11/22/24.
+//
+
+import Testing
+@testable import stock_cash
+
+struct stock_cashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
