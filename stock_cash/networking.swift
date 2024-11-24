855c80f154457be92f7e767caffd8dbdda362b99
John Andrews <john@soothsayerlabs.com>
2024-11-24 13:43:50 -0600

    update StockService to use Network Manager. Cleaner separation of concerns

--- stock_cash/networking.swift
+++ stock_cash/networking.swift
@@ -8,24 +8,12 @@
 import Foundation
 
 class StockService {
-    func fetchStocks(from url: URL, completion: @escaping (Result<[Stock], Error>) -> Void) {
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-            guard let data = data else {
-                completion(.failure(NSError(domain: "No data", code: 0)))
-                return
-            }
-            do {
-                let portfolio = try JSONDecoder().decode(Portfolio.self, from: data)
-                completion(.success(portfolio.stocks))
-            } catch {
-                completion(.failure(error))
-            }
+    class StockService {
+        private let networkManager = NetworkManager()
+
+        func fetchStocks(from url: URL, completion: @escaping (Result<[Stock], Error>) -> Void) {
+            networkManager.request(url, completion: completion)
         }
-        task.resume()
     }
 }
 
