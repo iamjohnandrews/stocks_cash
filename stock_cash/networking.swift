93b82d992811147c2ceed6a0b83de3e027de5cee
John Andrews <john@soothsayerlabs.com>
2024-11-24 13:54:03 -0600

    Cache the last successful API response locally to provide data when offline

--- stock_cash/networking.swift
+++ stock_cash/networking.swift
@@ -8,14 +8,38 @@
 import Foundation
 
 class StockService {
-    class StockService {
-        private let networkManager = NetworkManager()
+    private let cacheKey = "cachedStocks"
 
-        func fetchStocks(from url: URL, completion: @escaping (Result<[Stock], Error>) -> Void) {
-            networkManager.request(url, completion: completion)
+    func fetchStocks(from url: URL, completion: @escaping (Result<[Stock], Error>) -> Void) {
+        NetworkManager().request(url) { [weak self] (result: Result<[Stock], Error>) in
+            switch result {
+            case .success(let stocks):
+                self?.cacheStocks(stocks)
+                completion(.success(stocks))
+            case .failure(let error):
+                if let cachedStocks = self?.getCachedStocks() {
+                    completion(.success(cachedStocks))
+                } else {
+                    completion(.failure(error))
+                }
+            }
         }
     }
+
+    private func cacheStocks(_ stocks: [Stock]) {
+        if let data = try? JSONEncoder().encode(stocks) {
+            UserDefaults.standard.set(data, forKey: cacheKey)
+        }
+    }
+
+    private func getCachedStocks() -> [Stock]? {
+        if let data = UserDefaults.standard.data(forKey: cacheKey) {
+            return try? JSONDecoder().decode([Stock].self, from: data)
+        }
+        return nil
+    }
 }
+
 
 class NetworkManager {
     func request<T: Decodable>(_ url: URL, completion: @escaping (Result<T, Error>) -> Void) {
