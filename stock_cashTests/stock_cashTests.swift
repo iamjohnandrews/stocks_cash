940dec71f86cc01eed9c7ce61e26352706ebdb2b
John Andrews <john@soothsayerlabs.com>
2024-11-24 13:59:34 -0600

    test error state

--- stock_cashTests/stock_cashTests.swift
+++ stock_cashTests/stock_cashTests.swift
@@ -35,7 +35,17 @@ class StockViewModelTests: XCTestCase {
     func testViewModelHandlesEmptyState() {
         let viewModel = StockViewModel()
         viewModel.state = .empty
-//        XCTAssertEqual(viewModel.state, .empty, "ViewModel should correctly represent the empty state")
+        XCTAssertEqual(viewModel.state, .empty, "ViewModel should correctly represent the empty state")
     }
+    
+    func testViewModelHandlesErrorState() {
+        let mockService = MockStockService(error: NSError(domain: "Test", code: 0))
+        let viewModel = StockViewModel(service: mockService)
+
+        viewModel.fetchStocks()
+
+        XCTAssertEqual(viewModel.state, .error("Test"))
+    }
+
 }
 
