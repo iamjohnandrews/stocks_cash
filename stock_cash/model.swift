7f28ff56505f17801e0ff287c273232ed270f750
John Andrews <john@soothsayerlabs.com>
2024-11-24 18:02:54 -0600

    delete unsued model objects

--- stock_cash/model.swift
+++ stock_cash/model.swift
@@ -13,16 +13,12 @@ struct Stock: Codable, Identifiable {
     let name: String
     let currency: String
     let currentPriceCents: Int
-    let quantity: Int?
-    let currentPriceTimestamp: Int
     
     private enum CodingKeys: String, CodingKey {
             case ticker
             case name
             case currency
             case currentPriceCents = "current_price_cents"
-            case quantity
-            case currentPriceTimestamp = "current_price_timestamp"
         }
 }
 
